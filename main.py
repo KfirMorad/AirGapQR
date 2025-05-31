19328035a4065ef7ff0b74b4e5b4a82b55225945
Kfir Morad <kfir212160@gmail.com>
2025-06-01 00:48:59 +0300

    Refactor QR encoding process: remove chunker, integrate chunking into qr_encoder

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from encryptor import encrypt_file, decrypt_file
-from chunker import  chunk_file_base64
-from qr_encoder import save_qr_chunks
+from qr_encoder import encrypted_to_qr
 from qr_scanner import scan_qr_chunks
 
 encrypt_file("secret.txt", "password123")
-chunks = chunk_file_base64("encrypted.bin")
-save_qr_chunks(chunks)
+encrypted_to_qr("encrypted.bin")
 decrypt_file(scan_qr_chunks(), "password123")
--- qr_encoder.py
+++ qr_encoder.py
@@ -1,7 +1,21 @@
 import qrcode
 import os
+import base64
+def chunk_file_base64(path: str, chunk_size: int = 600) -> list[str]:
+    with open(path, "rb") as file:
+        binary_data = file.read()
 
-def save_qr_chunks(chunks, output_folder="qr_output"):
+    # Convert to base64 text (so we can store in QR)
+    b64_data = base64.b64encode(binary_data).decode()
+
+    # Slice into chunks
+    chunks = [b64_data[i:i + chunk_size] for i in range(0, len(b64_data), chunk_size)]
+
+    return chunks
+
+def encrypted_to_qr(path: str, output_folder="qr_output"):
+
+    chunks = chunk_file_base64(path)
     # Create the folder if it doesn't exist
     if not os.path.exists(output_folder):
         os.makedirs(output_folder)
