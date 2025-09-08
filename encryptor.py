e264e646b065fff652a1cf32977f5121c9890c7b
KfirMorad <130757840+KfirMorad@users.noreply.github.com>
2025-09-08 14:59:28 +0300

    Update encryptor.py

--- encryptor.py
+++ encryptor.py
@@ -9,22 +9,18 @@ def derive_key_from_password(password: str) -> bytes:
     return key
 
 def encrypt_file(input_path: str, password: str, output_path :str = "encrypted.bin"):
-    # 1. Derive encryption key from password
     key = derive_key_from_password(password)
     fernet = Fernet(key)
 
-    # 2. Read input file as bytes
     with open(input_path, "rb") as file:
         data = file.read()
 
-    # 3. Encrypt the data
     encrypted = fernet.encrypt(data)
 
-    # 4. Save the encrypted binary to output_path
     with open(output_path, "wb") as file:
         file.write(encrypted)
 
-    print(f"✅ File encrypted and saved to {output_path}")
+    print(f"File encrypted and saved to {output_path}")
 
 def decrypt_file(data: bytes, password: str, output_path: str = "decrypted.bin"):
     key = derive_key_from_password(password)
@@ -33,10 +29,10 @@ def decrypt_file(data: bytes, password: str, output_path: str = "decrypted.bin")
     try:
         decrypted = fernet.decrypt(data)  # decrypt raw bytes
     except Exception as e:
-        print("❌ Decryption failed - " + str(e))
+        print("Decryption failed - " + str(e))
         return
 
     with open(output_path, "wb") as file:
         file.write(decrypted)
 
-    print(f"✅ File decrypted and saved to {output_path}")
+    print(f"File decrypted and saved to {output_path}")
